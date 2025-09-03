760f9b1227f6fc41c94032050dd245087716b06d
1024小神 <15670339118@163.com>
2025-09-03 16:22:46 +0800

    Add image upload functionality to home page and adjust layout styles for better responsiveness

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -6,11 +6,26 @@
         <router-link to="/transparentPricing">transparentPricing</router-link>
         <router-link to="/about">about</router-link>
         <router-link to="/404">404</router-link>
+        <!-- 图片 -->
+        <input type="file" @change="handleImageChange" />
+        <!-- 显示图片 -->
+        <img :src="imageUrl" alt="image" class="imgStyle" />
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
+const imageUrl = ref('')
+
+const handleImageChange = (e: Event) => {
+    console.log('handleImageChange', e)
+    const file = (e.target as HTMLInputElement).files?.[0]
+    if (file) {
+        console.log('file', file)
+        imageUrl.value = URL.createObjectURL(file)
+    }
+}
 </script>
 
 <style scoped lang="scss">
@@ -19,7 +34,12 @@ import { ref } from 'vue'
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    height: 110vh;
     background-color: green;
+}
+
+.imgStyle {
+    width: 100px;
+    height: 100px;
 }
 </style>
